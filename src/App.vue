8bebf82664fb7063db275bbff67bbdb674c3bda6
AliceColombari <colombari.23@gmail.com>
2022-03-03 12:22:20 +0100

    base generale + importato bootstrap + fontsource + prove checkup

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app" class="container text-center">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1 class="debug">Prova debug font quicksand</h1>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './style/general.scss';
+
+  .debug {
+    font-family: "quicksand";
+  }
+</style>
