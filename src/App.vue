2b12747909154c7b655035c24032673db64fba2b
AliceColombari <colombari.23@gmail.com>
2022-03-03 14:30:59 +0100

    problema con collegamento app.vue

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,24 @@
   <div id="app">
     <!-- importo tag file .vue collegati -->
     <MyHeader/>
+    <MyMain/>
+    
   </div>
 </template>
 
 <script>
 // importo file .vue collegati
-import MyHeader from './components/MyHeader.vue'
+import MyHeader from './components/MyHeader.vue';
+import MyMain from './components/MyMain.vue';
+
 
 export default {
   name: 'App',
   components: {
     // importo nome file .vue collegati
     MyHeader,
+    MyMain
+ 
   }
 }
 </script>
---
+++ src/components/MyMain.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+     <h1>prova</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "MyMain"
+
+}
+</script>
+
+<style lang="scss">
+    h1 {
+        margin-top: 200px;
+    }
+</style>
